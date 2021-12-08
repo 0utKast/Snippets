f0f885e3e736c00877b3d9a1c4d351f7491abdf0
Jesus Conde <educaleft@gmail.com>
2021-12-08 14:38:42 +0000

    Agregar archivos de proyecto.

---
+++ Snippets/Snippets.cpp
@@ -0,0 +1,15 @@
+// Snippets.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+__interface IInterface
+{
+
+};
+int main()
+{
+    setlocale(LC_ALL, "es_ES.UTF-8");
+
+    std::cout << "Hello World!\n";
+}
+
